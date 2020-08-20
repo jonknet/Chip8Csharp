a5046c0b65dbe53680bbbf4989298f513d8dbd3f
Jon Kuzmick <jonknet@gmail.com>
2020-08-19 21:16:52 -0400

    draws in a window now!

--- Chip8Emu/App.xaml.cs
+++ Chip8Emu/App.xaml.cs
@@ -3,7 +3,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Drawing;
 using System.Threading;
-using SkiaSharp;
+
+
 
 
 namespace Chip8Emu
@@ -24,6 +25,14 @@ namespace Chip8Emu
             
             Loader.Load(path,mem);
             
+            var gWind = new GameWindow();
+
+            for(int i = 0; i < 32; i++)
+            {
+                gWind.drawPixel(0,i, true);
+            }
+            gWind.updateWindow();
+            
             // Main Loop
             while(!keypad.quitReceived){
                 keypad.pollKeyState();
@@ -32,13 +41,24 @@ namespace Chip8Emu
 
                 if(vram.change)
                 {
-                    Console.Clear();
-                    vram._outputToConsole();
+                    for(int i = 0; i < 32; i++)
+                    {
+                        for(int j = 0; j < 64; j++)
+                        {
+                            if(vram.getPixel(j,i) == 1)
+                                gWind.drawPixel(j,i, true);
+                            else
+                                gWind.drawPixel(j,i,false);
+                        }
+                    }
                     vram.change = false;
                 }
                 
-                Thread.Sleep(500);
+                gWind.updateWindow();
+                
+                Thread.Sleep(50);
             }
+            
             this.Shutdown();
         }
         
---
+++ Chip8Emu/GameWindow.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using Window = SFML.Window.Window;
+
+namespace Chip8Emu
+{
+    public class GameWindow 
+    {
+        
+        RenderWindow hWind;
+        const int width = 320;
+        const int height = 160;
+        
+        RectangleShape blackRect;
+        RectangleShape whiteRect;
+        
+        List<RectangleShape>[] rectArray; 
+
+        public GameWindow() 
+        {
+            hWind = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(320, 160), "Chip8 in C#");
+            hWind.Clear();
+            rectArray = new List<RectangleShape>[32];
+            for(int i = 0; i < 32; i++)
+            {
+                rectArray[i] = new List<RectangleShape>(64);
+                for(int j = 0; j < 64; j++)
+                {
+                    var rect = new RectangleShape(new Vector2f(5,5)){ FillColor = Color.White };
+                    rect.Position = new Vector2f(j*5,i*5);
+                    rectArray[i].Add(rect);
+                    hWind.Draw(rect);
+                    
+                }
+            }
+            hWind.Display();
+            
+        }
+        
+        public void updateWindow()
+        {
+            hWind.Clear();
+            
+            for(int i = 0; i < 32; i++)
+            {
+                foreach(RectangleShape r in rectArray[i])
+                {
+                    hWind.Draw(r);
+                }
+            }
+            
+            hWind.DispatchEvents();
+            hWind.Display();
+        }
+        
+        public void drawPixel(int x, int y, bool black)
+        {
+            
+            var rect = rectArray[y][x];
+            if(black){
+                rect.FillColor = Color.Black;
+            } else {
+                rect.FillColor = Color.White;
+            }
+            
+            //hWind.Display();
+            
+        }
+        
+    }
+}
